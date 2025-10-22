41e5d3d9dca08352f7a9e8a54d7ee1e8b0568526
beatriz-nagae <bh.prestashop@gmail.com>
2025-10-21 23:56:38 -0300

    UPDATE classes packets added

--- src/Principal.java
+++ src/Principal.java
@@ -1,20 +1,40 @@
+import br.com.alura.screenmatch.calculo.CalculadoraDeTempo;
 import br.com.alura.screenmatch.modelos.Filme;
+import br.com.alura.screenmatch.modelos.Serie;
 
 public class Principal {
     public static void main(String[] args) {
-        Filme filmeUm = new Filme();
-        filmeUm.setNome("Fame");
+        Filme favorito = new Filme();
+        favorito.setNome("The Matrix");
+        favorito.setAnoDeLancamento(1999);
+        favorito.setDuracaoEmMinutos(135);
+        favorito.setIncluidoNoPlano(true);
 
-        filmeUm.setIncluidoNoPlano(true);
-        filmeUm.setDuracaoEmMinutos(160);
+        Filme outro = new Filme();
+        outro.setNome("John Wick");
+        outro.setAnoDeLancamento(2014);
+        outro.setDuracaoEmMinutos(101);
+        outro.setIncluidoNoPlano(true);
 
-        filmeUm.exibeFichaTecnica();
-        filmeUm.avalia(8);
-        filmeUm.avalia(3);
-        filmeUm.avalia(10);
+        Serie serie = new Serie();
+        serie.setNome("La Casa de Papel");
+        serie.setAnoDeLancamento(2017);
+        serie.setIncluidoNoPlano(true);
+        serie.setAtiva(true);
+        serie.setTemporadas(5);
+        serie.setEpisodiosPorTemporada(10);
+        serie.setMinutosPorEpisodio(45);
 
-       // System.out.println(filmeUm.somaAvaliacoes);
-        System.out.println("Total: " + filmeUm.getTotalDeAvaliacoes());
-        System.out.println(filmeUm.pegaMediaDasAvaliacoes());
+        CalculadoraDeTempo calculadora = new CalculadoraDeTempo();
+        calculadora.inclui(favorito);
+        calculadora.inclui(outro);
+        calculadora.inclui(serie);
+
+        System.out.println("Tempo total: " +calculadora.getTempoTotal());
+        System.out.println("Filme favorito: " + favorito.getNome());
+        System.out.println("Outro filme: " + outro.getNome());
+        System.out.println("Série: " + serie.getNome());
+        System.out.println("Tempo total: " + calculadora.getTempoTotal());
+
     }
 }
--- src/br/com/alura/screenmatch/modelos/Filme.java
+++ src/br/com/alura/screenmatch/modelos/Filme.java
@@ -1,62 +1,15 @@
 package br.com.alura.screenmatch.modelos;
 
-public class Filme {
-    private String nome;
-    private int anoDeLancamento;
-    private boolean incluidoNoPlano;
-   private double somaAvaliacoes; //shift + f6 pra renomear tudo
-    private int totalDeAvaliacoes;
-    private int duracaoEmMinutos;
+public class Filme extends Titulo {
 
+    private String diretor;
 
-    //getters
-  public int getTotalDeAvaliacoes(){
-        return totalDeAvaliacoes;
+    public String getDiretor() {
+        return this.diretor;
     }
 
-    public String getNome() {
-        return nome;
-    }
-
-    public int getAnoDeLancamento() {
-        return anoDeLancamento;
-    }
-
-    public boolean isIncluidoNoPlano() {
-        return incluidoNoPlano;
-    }
-
-    public int getDuracaoEmMinutos() {
-        return duracaoEmMinutos;
-    }
-
-    //setters
-    public void setNome(String nome) {
-        this.nome = nome; //O this quer dizer ESTE objeto. Ref magica
-    }
-
-    public void setIncluidoNoPlano(boolean incluidoNoPlano) {
-        this.incluidoNoPlano = incluidoNoPlano;
-    }
-
-    public void setDuracaoEmMinutos(int duracaoEmMinutos) {
-        this.duracaoEmMinutos = duracaoEmMinutos;
-    }
-
-
-    //metodos
-    public void exibeFichaTecnica(){
-        System.out.println("Nome do filme: " + nome);
-        System.out.println("Ano de lançamento: " + anoDeLancamento);
-    }
-
-    public void avalia(double nota){ //nota é um parametro do metodo
-        somaAvaliacoes += nota;
-        totalDeAvaliacoes++;
-    }
-
-   public double pegaMediaDasAvaliacoes(){ //usa Double porque ele vai retornar algo que e double
-        return somaAvaliacoes / totalDeAvaliacoes;
+    public void setDiretor(String diretor) {
+        this.diretor = diretor;
     }
 
 }
